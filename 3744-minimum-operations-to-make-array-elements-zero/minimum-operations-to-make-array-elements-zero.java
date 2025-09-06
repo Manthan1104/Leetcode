d98a45984dd1f34a92d51e089441a59ce52e56a9
Manthan1104 <156171023+Manthan1104@users.noreply.github.com>
2025-09-07 00:23:10 +0530

    Time: 20 ms (22.22%) | Memory: 100.9 MB (77.78%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
@@ -1,20 +1,23 @@
 class Solution {
-    static long expSum4[]=new long[18];
-    static long expSum(int x){
-        if (x==0) return 0;
-        int log4=(31-Integer.numberOfLeadingZeros(x))>>1;
-        int r=x-(1<<(log4<<1));
-        return expSum4[log4]+r*(log4+1L);
-    }
-    static public long minOperations(int[][] queries) {
-        expSum4[0]=1;
-        for(int i=1; i<18; i++)
-            expSum4[i]=expSum4[i-1]+3L*i*(1L<<(2*(i-1)))+1;
-        long op=0;
-        for(int[] q : queries){
-            int l=q[0]-1, r=q[1];
-            op+=(expSum(r)-expSum(l)+1)>>1;// ceiling of (expSum(r)-expSum(l))/2
+    private long stepsum(long n) {
+        if (n <= 0) return 0;
+        long res = 0, base = 1, step = 1;
+        while (base <= n) {
+            long cnt = Math.min(n, base * 4 - 1) - base + 1;
+            res += cnt * step;
+            base *= 4;
+            step++;
         }
-        return op;
+        return res;
+    }
+
+    public long minOperations(int[][] queries) {
+        long ans = 0;
+        for (int[] q : queries) {
+            long l = q[0], r = q[1];
+            long total = stepsum(r) - stepsum(l - 1);
+            ans += (total + 1) / 2;
+        }
+        return ans;
     }
 }
