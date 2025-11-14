f54220805b3d58d96e4ae1dfda3f5968af41addb
Manthan1104 <156171023+Manthan1104@users.noreply.github.com>
2025-11-14 09:23:58 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (7.45%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,22 +1,42 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int col[]=new int[m];
-        int row[]=new int[n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int col0 = 1;
+
+        // Step 1: Mark the rows and columns using first row and first column
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;  // mark row
+                    matrix[0][j] = 0;  // mark column
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1 ||col[j]==1){
-                    matrix[i][j]=0;
+
+        // Step 2: Use markers to set zeroes (skip first row and first col)
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
+            }
+        }
+
+        // Step 3: Handle first row
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Handle first column
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
