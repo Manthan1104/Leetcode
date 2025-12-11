fbcf70bf2de0c2925d3f025803ed77782ed7568d
Manthan1104 <156171023+Manthan1104@users.noreply.github.com>
2025-12-11 22:49:55 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (28.24%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,12 +11,12 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Map<ListNode,Integer>mpp=new HashMap<>();
-        ListNode temp=head;
-        while(temp!=null){
-            if(mpp.containsKey(temp))return true;
-            mpp.put(temp,1);
-            temp=temp.next;
+       ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)return true;
         }
         return false;
     }
