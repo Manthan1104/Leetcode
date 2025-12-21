614ae49381584d421d287fe6d7b62e14309c622d
Manthan1104 <156171023+Manthan1104@users.noreply.github.com>
2025-12-21 10:04:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (20.55%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
@@ -3,47 +3,33 @@ class Solution {
         int n = strs.length;
         int m = strs[0].length();
 
-        // Convert strings to char arrays once
-        // This avoids repeated charAt() calls inside nested loops
-        char[][] a = new char[n][];
-        for (int i = 0; i < n; i++) {
-            a[i] = strs[i].toCharArray();
-        }
-
-        // resolved[i] = true means:
-        // strs[i] is already strictly smaller than strs[i+1]
-        // considering previously kept columns
-        boolean[] resolved = new boolean[n - 1];
-
-        // Number of adjacent row pairs whose order is still undecided
-        int unresolved = n - 1;
-
+        boolean[] sorted = new boolean[n - 1];
         int deletions = 0;
 
-        // Process columns left to right
-        for (int col = 0; col < m && unresolved > 0; col++) {
+        for (int col = 0; col < m; col++) {
             boolean needDelete = false;
 
-            // Check if keeping this column breaks lexicographical order
+            // Check if this column breaks order
             for (int row = 0; row < n - 1; row++) {
-                // Only compare rows whose order is not yet fixed
-                if (!resolved[row] && a[row][col] > a[row + 1][col]) {
-                    needDelete = true;
-                    break;
+                if (!sorted[row]) {
+                    if (strs[row].charAt(col) > strs[row + 1].charAt(col)) {
+                        needDelete = true;
+                        break;
+                    }
                 }
             }
 
-            // If this column violates order, delete it
+            // If invalid, delete this column
             if (needDelete) {
                 deletions++;
                 continue;
             }
 
-            // Otherwise, update which row pairs become strictly ordered
+            // Update sorted status
             for (int row = 0; row < n - 1; row++) {
-                if (!resolved[row] && a[row][col] < a[row + 1][col]) {
-                    resolved[row] = true;
-                    unresolved--;
+                if (!sorted[row] &&
+                    strs[row].charAt(col) < strs[row + 1].charAt(col)) {
+                    sorted[row] = true;
                 }
             }
         }
