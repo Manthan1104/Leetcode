84f5972d5c093b25e00412fdcc58d162b54200fa
Manthan1104 <156171023+Manthan1104@users.noreply.github.com>
2026-03-11 14:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (92.54%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -1,37 +1,14 @@
 class Solution {
-    Integer dp[][]=new Integer[101][10001];
-   
-    public int superEggDrop(int K, int N) {
-      
-        if(K==1)
-            return N;
-        
-        if(N==0 || N==1)
-            return N;
-        
-        if(dp[K][N]!=null)
-            return dp[K][N];
-        
-        int i,l=1,h=N;
-        int ans=Integer.MAX_VALUE;
+    public int superEggDrop(int k, int n) {
+        int[] dp = new int[k+1];
+        int moves = 0;
 
-        while(l<=h)
-        {
-            int mid=(l+h)/2;
-            
-            int down_temp=superEggDrop(K-1,mid-1);
-            
-            int up_temp=superEggDrop(K,N-mid);
-            
-            int temp=1+Math.max(down_temp,up_temp);
-           
-            if(down_temp<up_temp)
-                l=mid+1;
-            else 
-                h=mid-1;
-            
-            ans=Math.min(temp,ans);
+        while(dp[k] < n){
+            moves++;
+            for(int i = k; i >= 1; i--){
+                dp[i] = dp[i] + dp[i-1] + 1;
+            }
         }
-        return dp[K][N]=ans;
+        return moves;
     }
 }
