a3c8672d91508417c0aa1a9f8177e5ca480d7883
Manthan1104 <156171023+Manthan1104@users.noreply.github.com>
2026-03-13 09:27:00 +0530

    Time: 8 ms (84.85%) | Memory: 47.8 MB (35.35%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,55 +1,29 @@
-import java.util.Arrays;
-
 class Solution {
     public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-        Arrays.sort(workerTimes);
-        
-        long lowTime = 0;
-        long highTime = (long) 1e16; 
-        long minRequiredTime = highTime;
-        
-        // Outer Binary Search: Finding the minimum required time
-        while (lowTime <= highTime) {
-            long midTime = lowTime + (highTime - lowTime) / 2;
-            long totalHeightReduced = 0;
-            
-            for (int baseTime : workerTimes) {
-                // Inner Binary Search: Finding max height THIS worker can reduce in 'midTime'
-                long lowHeight = 0;
-                long highHeight = mountainHeight;
-                long maxWorkerHeight = 0;
-                
-                while (lowHeight <= highHeight) {
-                    long midHeight = lowHeight + (highHeight - lowHeight) / 2;
-                    
-                    // Crucial: Cast to long BEFORE multiplying to prevent 32-bit overflow
-                    long timeNeeded = (long) baseTime * midHeight * (midHeight + 1) / 2;
-                    
-                    if (timeNeeded <= midTime) {
-                        maxWorkerHeight = midHeight;
-                        lowHeight = midHeight + 1;
-                    } else {
-                        highHeight = midHeight - 1;
-                    }
-                }
-                
-                totalHeightReduced += maxWorkerHeight;
-                
-                // Early Exit Optimization
-                if (totalHeightReduced >= mountainHeight) {
-                    break;
-                }
-            }
-            
-            // Adjust outer binary search bounds
-            if (totalHeightReduced >= mountainHeight) {
-                minRequiredTime = midTime;
-                highTime = midTime - 1;
-            } else {
-                lowTime = midTime + 1;
+        long left = 0;
+        long right = (long)1e18;
+        long ans =  right;
+        while(left<=right){
+            long mid =  left+(right-left)/2;
+            if(solve(mid,mountainHeight, workerTimes)){
+                ans =  mid;
+                right =  mid-1;
+            }  else{
+                left =  mid+1;
             }
         }
-        
-        return minRequiredTime;
+        return ans;
+    }
+    public boolean solve(long time,int height, int[] workerTimes){
+        long total = 0;
+        for(int t:workerTimes ){
+            long val  =  (2*time)/t;
+            long x = (long)((Math.sqrt(1 + 4 * val) - 1) / 2);
+            total+=x;
+            if(total>=height){
+                return true;
+            }
+        }
+           return false;
     }
 }
