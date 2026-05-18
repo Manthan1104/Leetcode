e0388e4c41360050da1778389c157e7e7f80be3a
Manthan1104 <156171023+Manthan1104@users.noreply.github.com>
2026-05-19 03:20:01 +0530

    Time: 57 ms (52.67%) | Memory: 80.8 MB (61.58%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -1,48 +1,40 @@
-import java.util.*;
-
 class Solution {
     public int minJumps(int[] arr) {
         int n = arr.length;
-        if (n == 1) return 0;
-
-        Map<Integer, List<Integer>> mp = new HashMap<>();
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            mp.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+            map.computeIfAbsent(arr[i], e -> new ArrayList<>()).add(i);
         }
-
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-
-        boolean[] vis = new boolean[n];
-        vis[0] = true;
-
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int node = curr[0];
-            int dist = curr[1];
-
-            if (node == n - 1) return dist;
-
-            if (node - 1 >= 0 && !vis[node - 1]) {
-                vis[node - 1] = true;
-                q.offer(new int[]{node - 1, dist + 1});
-            }
-
-            if (node + 1 < n && !vis[node + 1]) {
-                vis[node + 1] = true;
-                q.offer(new int[]{node + 1, dist + 1});
-            }
-
-            for (int next : mp.get(arr[node])) {
-                if (!vis[next]) {
-                    vis[next] = true;
-                    q.offer(new int[]{next, dist + 1});
+        queue.offer(0);
+        visited[0] = true;
+        int jump = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int pos = queue.poll();
+                if (pos == n-1) return jump;
+                if (pos + 1 < n && !visited[pos + 1]) {
+                    queue.offer(pos + 1);
+                    visited[pos + 1] = true;
+                }
+                if (pos - 1 >= 0 && !visited[pos - 1]) {
+                    queue.offer(pos - 1);
+                    visited[pos - 1] = true;
+                }
+                if (map.containsKey(arr[pos])) {
+                    for (int index: map.get(arr[pos])) {
+                        if (!visited[index]) {
+                            queue.offer(index);
+                            visited[index] = true;
+                        }
+                    }
+                    map.remove(arr[pos]);
                 }
             }
-
-            mp.get(arr[node]).clear();
+            jump++;
         }
-
-        return -1;
+        return jump;
     }
 }
